380b0096bcc149ed27f96cc928b3cece90eba564
77001 <alimzhanzhakashev@gmail.com>
2023-01-21 15:15:33 +0600

    init comit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,29 @@
-console.log('hello world!');
+const authBtn = document.getElementById('open-auth-btn')  ///войти 
+const modal = document.getElementById('auth-modal') // модальное окно
+const closeBtn = modal.querySelectorAll('.close-btn') //выход 
+
+
+const openModal = () =>{
+    authBtn.addEventListener('click' , () => {
+        modal.style.display = 'block'
+    
+        setTimeout(() =>{
+            modal.classList.add('show')
+        }, 300)
+    })
+}
+
+const closeModal = () =>{
+    modal.classList.remove('show')
+    
+
+    setTimeout(()=>{
+        modal.style.display = 'none'
+    }, 300)
+}
+
+authBtn.addEventListener('click' , openModal)
+
+closeBtn.forEach((btn) => {
+    btn.addEventListener('click', closeModal)
+})
